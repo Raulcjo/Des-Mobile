8ad61a9591853da17c82d53db1610fc0571f54da
Raul A. Costa <113145571+Raulcjo@users.noreply.github.com>
2023-09-05 21:48:51 -0300

    Aula 11 - 05/09/2023

---
+++ Aula11-DesMobile/src/components/TextoCentral.js
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native"
+
+
+export default props => {
+    <View style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: props.corFundo || '#000'
+                    }}>
+        <Text style={{fontSize: 40, color: props.corTexto || '#fff'}}>
+            {props.children}
+        </Text>
+    </View>
+}
---
+++ Aula11-DesMobile/src/views/TelaB.js
@@ -0,0 +1,8 @@
+import TextoCentral from "../components/TextoCentral"
+
+
+export default props => {
+    <TextoCentral corFundo='#338264'>
+        Tela B
+    </TextoCentral>
+}
